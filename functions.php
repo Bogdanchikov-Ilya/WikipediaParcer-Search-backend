f39f66003ee8f63b41f61e0239095eb30dd5e920
Bogdanchikov-Ilya <test200373@gmail.com>
2021-09-21 20:37:36 +0300

    test search

--- functions.php
+++ functions.php
@@ -15,15 +15,16 @@ function addArticles($connect, $data) {
     $body = $data['body'];
 
     $body = mb_strimwidth($body, 0, 50000, "..."); // обрезаю т.к если много символов в бд не добавляется
+    $body = str_replace(array("?","!",",",";",".","-","(",")","—"), "", $body);
+    $body = preg_replace('/\s+/', ' ', $body);
+    $body = mb_strtolower($body);
+
     $url = $data['url'];
     $size = $data['size'];
     $count_words = $data['count_words'];
 
     mysqli_query($connect, "INSERT INTO `articles` (`title`, `body`, `url`, `size`, `count_words`) VALUES ('$title', '$body', '$url', '$size', '$count_words')");
 
-    $body = str_replace(array("?","!",",",";",".","-","(",")","—"), "", $body);
-    $body = preg_replace('/\s+/', ' ', $body);
-    $body = mb_strtolower($body);
     $body=explode(' ',$body);
 
     foreach($body as $value) {
@@ -40,41 +41,81 @@ function addArticles($connect, $data) {
     echo json_encode($res);
 }
 
-//function search($connect, $data) {
-//    $searchValue = $data['text'];
-//    $searchValue= mb_strtolower($searchValue);
-//
-//    //получаю id введенного слова
-//    $wordsId = mysqli_query($connect, "SELECT `id` FROM `words` WHERE `text` = '$searchValue'");
-//    $wordsId = mysqli_fetch_assoc($wordsId);
-//
-//    var_dump($wordsId);
-//    // пушу в результирующую таблицу айти слова
-//
-//    mysqli_query($connect, "INSERT INTO `articles_words` (`articles_id`, `words_id`, `counter`) VALUES (10, 10, 10)");
-//    // получаю массив айдишников статей со словом $searchValue
-//    $articleIdArr = mysqli_query($connect, "SELECT `id` FROM `articles` WHERE `body` LIKE '%$searchValue%'");
-//    $articleIdArr = mysqli_fetch_array($articleIdArr);
-//    // пушу массив айдишников в связующую таблицу
-//    mysqli_query($connect, "INSERT INTO `articles_words` (`articles_id`) VALUES ('$articleIdArr')");
-//    // делаю запрос с каждым из айдишников и беру текст боди
-//    $items = [];
-//    foreach ($articleIdArr as $value) {
-//        $items[] = mysqli_query($connect, "SELECT `body` FROM `articles` WHERE `id` = '$value'");
-//
-//    }
-//    var_dump($items);
-//    function getCount($text, $searchValue) {
-//        return substr_count($text, $searchValue);
-//    }
-//    $textCountArray = [];
-//    for ($i = 0; $i < count($items); $i++){
-//        $textCountArray[$i] = getCount($items[$i], $searchValue);
-//    };
-//    // пушу массив с кол-вом вхождений в результирующую таблицу
-//    mysqli_query($connect, "INSERT INTO `articles_words` (`counter`) VALUES ('$textCountArray')");
-//    $res = mysqli_query($connect, "SELECT * FROM `articles_words`");
-//
-//
-////    echo json_encode($res);
-//}
+function search($connect, $data) {
+    // обработка введенного слова
+    $searchValue = $data['text'];
+    $searchValue= mb_strtolower($searchValue);
+
+
+    //получаю id введенного слова
+    $wordsId = mysqli_query($connect, "SELECT `id` FROM `words` WHERE `text` = '$searchValue'");
+    $wordsId = mysqli_fetch_all($wordsId, MYSQLI_ASSOC);
+    foreach ($wordsId as $row) {
+        $wordsId = $row["id"];
+    }
+    $wordsId = intval($wordsId);
+
+    // пушу в результирующую таблицу айди слова
+    // mysqli_query($connect, "INSERT INTO `articles_words` (`words_id`, `counter`) VALUES ('$wordsId', NULL)");
+
+    // получаю массив айдишников статей со словом $searchValue
+    $getArticleId = mysqli_query($connect, "SELECT `id` FROM `articles` WHERE `body` LIKE '%в%'");
+    $articleIdArray = [];
+    while ($row = mysqli_fetch_assoc($getArticleId)) {
+        $articleIdArray[] = intval($row["id"]);
+    }
+    $articleIdArray = json_encode($articleIdArray);
+
+
+    // пушу массив айдишников в связующую таблицу
+    // mysqli_query($connect, "INSERT INTO `articles_words` (`articles_id`) VALUES ('$articleIdArray')");
+
+
+    // делаю запрос с каждым из айдишников и беру текст боди
+    $textArticlesArray = [];
+    $articleIdArray = json_decode($articleIdArray);
+    foreach ($articleIdArray as $value) {
+        $item = mysqli_query($connect, "SELECT `body` FROM `articles` WHERE `id` = '$value'");
+        $item = mysqli_fetch_assoc($item);
+        $textArticlesArray[] = $item;
+    }
+    $articleIdArray = json_encode($articleIdArray);
+
+
+    // подсчитываю кол-во повторений слова в каждом эелемнте массива $textArticlesArray
+    $counterArray = [];
+    foreach ($textArticlesArray as $row) {
+        $counterArray[] = substr_count($row["body"], $searchValue);
+    }
+    $counterArray = json_encode($counterArray);
+
+    //пушу массив с счтчиками в связующую таблицу
+    mysqli_query($connect, "INSERT INTO `articles_words` (`articles_id`, `words_id`, `counter`) VALUES ('$articleIdArray', '$wordsId', '$counterArray')");
+//    print_r($articleIdArray);
+//    print_r($wordsId);
+//    print_r($counterArray);
+
+    $articleIdArray = json_decode($articleIdArray);
+    $counterArray = json_decode($counterArray);
+    $titlesArray = [];
+    $res = [];
+
+
+
+    for ($i = 0; $i < count($articleIdArray); $i++) {
+
+        $title = mysqli_query($connect, "SELECT `title` FROM `articles` WHERE `id` = $articleIdArray[$i]");
+        $titlesArray[] = mysqli_fetch_assoc($title);
+
+        // SELECT `counter` FROM `articles_id` WHERE `counter` = $counterArray[$is]
+
+        $res[] = ["title" => $titlesArray[$i],
+                    "counter" => $counterArray[$i]];
+
+    }
+    echo json_encode($res);
+    die();
+
+    // беру название статиь и количество совпадений и отдаю на фронт
+
+}
